2df66ca4f5dc435b3dfd6110cd3bd00f4e01f051
Nastya991 <kim-dem@mail.ru>
2015-05-06 19:21:54 +0300

    Revert "щ"
    This reverts commit e802255bc2d4ca6e2d380fa90a1fe266c9b164e0.

--- Untitled.m
+++ Untitled.m
@@ -2,32 +2,31 @@ close all;
 clear all;
 clc;
 
-A(1) = 0;
-A(2) = 0;
-A(3) = 0;
-A(4) = 1;
-A(5) = 0.5;
-A(6) = 1/6;
-A(7) = 0;
-A(8) = 0;
-G = linspase(0,20,9); 
-D = (0.75*sigma(4,4)+0.375*sigma(5,5)+0.125*sigma(6,6))*1/6;
-G*A(n)*sigma(n,n)- D*G + sigma(n,n)=0.125;
+k = 0.2337;
+T = 100;
+n = 60;
+t=linspace(1,100,n);% t values
+u=exp(-t);
+u1=-k*(T-u); %model
 
-for i = 1:8;
-for j = 1:8;
-if i = j 
-    D = (0.75*sigma(i,j)+0.375*sigma(i,j)+0.125*sigma(i,j))*1/6;
-    sigma(i,j)=(0.125+ D*G)/(1+G*A(i));
-elseif sigma(i,j)= 0;
-end
-end
-end
+i=1:60;
+u=exp(-i);
+
+first_milk = u>=70;
+second_milk=u<70;
+
+if first_milk
+u1= -k*(T-u);
 
 figure;
 hold on;
-plot(sigma(i,j),G,'red'); 
+plot(t,u,'black')
 hold off;
-grid on;
-xlabel('G');
-ylabel('sigma');
+elseif second_milk
+u1=-k*(T-u);
+
+figure;
+hold on;
+plot(t,u,'red')
+hold off;
+end
--- optimization/DecreaseTemprature.m
+++
@@ -1,4 +0,0 @@
-function T = DecreaseTemprature(t,alfa)
-T = alfa*t;
-
-end
